931b83f0975be738b1d7942e90f5791d5d51a3ce
vardhah <harsh.vardhan@vanderbilt.edu>
2020-06-04 15:10:53 -0500

    working function based REBNN

--- REBNN_GPU/Matfunction/main.m
+++ REBNN_GPU/Matfunction/main.m
@@ -1,28 +1,30 @@
-%% Initialisation for simulation
+%% Initialisation for simulation ::::::::
+%Timing and simulation steps
 tic;
-time=10;
+time=50;
 sample_frequency= 1000;  % in 1/Hz
 
-%inputs to Nueral network
+% Inputs to Neural network
 u2=1;
 learning_rate=0.01;
 training=1;
-%inital state of system 
+
+% Inital state of system 
 y0=[0;0];                % inital position(theta,theta_dot)
-%input signal 
+% Input signal 
 input_freq= 1 ;           % in rad/sec
 amplitude=3.8  ;          % amplitude of sine wave
 
-%intialising buffer
+% Intialising buffer
 dif_ip=0;
 rbnn_output=0;
-%Intialisation ends here
+% Intialisation ends here :::::::::::
 
 %% Dynamics System simulation 
 tspan=0:(1/sample_frequency):(time*input_freq); 
 ft=tspan;
 f= 3.8*sin(tspan);
-[t,y]=ode45(@(t,y)pend_test(t,y,ft,f),tspan,y0);
+[t,y]=ode45(@(t,y)pendulum(t,y,ft,f),tspan,y0);
 sample_period=1/sample_frequency;
 theta=y(:,1);
 theta_dot=y(:,2);
@@ -36,31 +38,32 @@ y_rbnn=[];
 
 for i = 1: iteration 
     
+   % rezonator code (input creation and its recursive dft) 
     sin_fn=3.8*sin(input_ph);
     [rezo_ph,Fn]=rezonator(sin_fn);
+    
+   % Neural netwrok and related codes (for multi output system need to
+   % rewrite it with different input-output. )
     [output_difference_vector,output_fft]= difBuffer(dif_ip,rbnn_output);
     u1=output_fft ; df= output_difference_vector;
     nn_op= NN(u1,df,Fn,u2,learning_rate,training);
     freq_char=nn_op; dft= Fn;
     yn=transpose(freq_char)*dft;
-   
-  
-   
-   
-   
+    
+  %data storage for plotting 
    sin_holder=[sin_holder sin_fn];
    resonator_holder=[resonator_holder rezo_ph];
-   y_rbnn=[y_rbnn yn];
+   y_rbnn=[y_rbnn ; yn];
    
-   
-   
+  %simulation step increament
    input_ph=input_ph+sample_period; 
-   %Update buffer code(to write)
+  %Update buffer code(to write)
    dif_ip=theta(i+1)-yn;
    rbnn_output=yn;
-     
+   
 end
 
 figure;plot(sin_holder);hold on; plot(resonator_holder) ;
 figure; plot(theta);hold on; plot(y_rbnn) ;
+clear; 
 toc;
---
+++ REBNN_GPU/Matfunction/pendulum.m
@@ -0,0 +1,15 @@
+function dy = pendulum(t,y,ft,f)
+
+m = 0.2;
+L = 2;
+g = 10;
+b = 1;
+
+Sy = sin(y(1));
+D = m*L*L;
+
+f=interp1(ft,f,t);
+u=f;
+dy(1,1) = y(2);
+dy(2,1) = (1/D)*(-m*g*L*Sy - b*y(2)) +(1/D)*u;
+end
